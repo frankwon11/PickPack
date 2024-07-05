004c1d04f1d19817a91bf7f7ddfbb92bc4e2605e
frankwon11 <frankwon11@gmail.com>
2024-07-05 21:43:21 +0900

    Initial Commit

---
+++ PickPack/PickPackApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickPackApp.swift
+//  PickPack
+//
+//  Created by sseungwonnn on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct PickPackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

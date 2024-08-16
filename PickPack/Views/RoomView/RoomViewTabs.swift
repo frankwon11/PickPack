815e4ab5707404650ac81c8a1b607c4836263876
Lee Sihyeong <poeticform@10-141-55-243.0200.pos.kr.iosda.org>
2024-08-17 03:31:00 +0900

    resolve conflict

---
+++ PickPack/Views/RoomView/RoomViewTabs.swift
@@ -0,0 +1,14 @@
+//
+//  RoomViewTabs.swift
+//  PickPack
+//
+//  Created by Lee Sihyeong on 8/17/24.
+//
+
+import Foundation
+
+enum RoomViewTabs: String, CaseIterable {
+    case MyItem = "내 짐"
+    case FriendItem = "친구 짐"
+    case SharedItem = "공유 짐"
+}

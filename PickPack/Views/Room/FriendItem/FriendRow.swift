8c79a506489b3d30b69afc4db222c1ce7454bf85
VronzeWook <chanel0222@naver.com>
2024-08-21 20:40:44 +0900

    [Design] #28 - FriendItemList 데이터 연동

---
+++ PickPack/Views/Room/FriendItem/FriendRow.swift
@@ -0,0 +1,76 @@
+//
+//  FriendRow.swift
+//  PickPack
+//
+//  Created by LDW on 8/17/24.
+//
+
+import SwiftUI
+
+struct FriendRow: View {
+    
+    @Binding var member: Member
+    
+    var body: some View {
+        HStack(spacing: 0){
+            
+            Image(systemName: "person.circle")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(member.color.color)
+                .frame(width: 40, height: 40)
+                .padding(.trailing, 16)
+            
+            Text(member.user.name)
+                .font(.body)
+            
+            Spacer()
+            
+            NavigationLink {
+                FriendItemListView(member: $member)
+            } label: {
+                HStack{
+                    Text("짐리스트 보러가기")
+                        .font(.subheadline)
+                    Image(systemName: "chevron.right")
+                    
+                }
+                .foregroundStyle(.black6)
+            }
+        }
+        .padding(.horizontal, 20)
+        .padding(.vertical, 16)
+        .background(.white) // 확인용, 나중에 white로
+        .cornerRadius(20)
+//        .onAppear {
+//            member.itemList[0].isShared = true
+//            member.itemList[1].isShared = true
+//            member.itemList[1].isPacked = true
+//            member.itemList[2].isShared = true
+//            member.itemList[3].isPacked = true
+//            member.itemList[4].isPacked = true
+//            
+//            print("\n")
+//            
+//            print("memeber item state")
+//            print("member.itemList[0].isPacked : \(member.itemList[0].isPacked)")
+//            print("member.itemList[0].isShared : \(member.itemList[0].isShared)")
+//            
+//            print("member.itemList[1].isPacked : \(member.itemList[1].isPacked)")
+//            print("member.itemList[1].isShared : \(member.itemList[1].isShared)")
+//        }
+    }
+}
+
+#Preview {
+    var list = ItemList().items
+    
+    list[0].isShared = true
+    list[1].isShared = true
+    list[1].isPacked = true
+    list[2].isShared = true
+    list[3].isPacked = true
+    list[4].isPacked = true
+    
+    return FriendRow(member: .constant( Member(id: "1234", user: User(id: "14142", name: "슌"), itemList: list, color: .indigo)))
+}

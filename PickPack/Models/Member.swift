408443b44b3e68501590302284ebe08490ae0a36
VronzeWook <chanel0222@naver.com>
2024-08-21 16:03:11 +0900

    [Feat] #23 - SharedItem 모델 생성

--- PickPack/Models/Member.swift
+++ PickPack/Models/Member.swift
@@ -5,7 +5,7 @@
 //  Created by LDW on 8/21/24.
 //
 
-import Foundation
+import SwiftUI
 
 struct Member: Identifiable, Codable, Hashable {
     let id: String
@@ -15,16 +15,30 @@ struct Member: Identifiable, Codable, Hashable {
     var itemList: [Item]
     
     var color: MemberColor
-    
-    
-    
 }
 
 enum MemberColor: String, Codable, Hashable {
-    case green
-    case till
-    case blue
-    case indigo
-    case purple
-    case pink
+    case green = "green"
+    case teal = "teal"
+    case blue = "blue"
+    case indigo = "indigo"
+    case purple = "purple"
+    case pink = "pink"
+    
+    var color: Color {
+        switch self {
+        case .green:
+            return Color.green
+        case .teal:
+            return Color.teal
+        case .blue:
+            return Color.blue
+        case .indigo:
+            return Color.indigo
+        case .purple:
+            return Color.purple
+        case .pink:
+            return Color.pink
+        }
+    }
 }
